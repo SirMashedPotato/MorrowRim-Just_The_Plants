7674653ddc5976841ee6e69290974bbe1ce3cf79
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2022-10-10 11:39:11 +1300

    Update to 1.0.1 (initial 1.4 update)

---
+++ 1.4/Source/MorrowRim_AshSwamp/MorrowRim_AshSwamp/Comps/Comp_PlantSpawner.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Verse;
+using RimWorld;
+using Verse.Sound;
+
+namespace MorrowRim_AshSwamp
+{
+    class Comp_PlantSpawner : ThingComp
+    {
+		public CompProperties_PlantSpawner Props
+		{
+			get
+			{
+				return (CompProperties_PlantSpawner)this.props;
+			}
+		}
+
+		public override void CompTickLong()
+		{
+			base.CompTickLong();
+			if (Props.thing != null)
+			{
+				Plant plant = parent as Plant;
+				if(plant.Growth >= Props.growthMin)
+				{
+					plant.Growth -= Props.growthDecrease;
+					DoSpawn(plant);
+
+
+				}
+			}
+		}
+
+		private void DoSpawn(Plant plant)
+		{
+			if (Props.sound != null)
+			{
+				Props.sound.PlayOneShot(new TargetInfo(plant.Position, plant.Map, false));
+			}
+			if (Props.radius == 0)
+			{
+				GenSpawn.Spawn(Props.thing, plant.Position, plant.Map);
+			}
+			else
+			{
+				int radius = GenRadial.NumCellsInRadius(Props.radius);
+				for (int i = 0; i != radius; i++)
+				{
+					IntVec3 tile = plant.Position + GenRadial.RadialPattern[i];
+					if (tile.InBounds(plant.Map))
+					{
+						GenSpawn.Spawn(Props.thing, tile, plant.Map);
+					}
+				}
+			}
+		}
+
+	}
+}
